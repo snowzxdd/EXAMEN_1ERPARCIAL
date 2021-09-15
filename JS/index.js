a0fcb60e43749b8f29e818266f55421be10f1ff3
Luis <snowzxdd@gmail.com>
2021-09-14 21:40:10 -0500

    Examen

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+let num = 0;
+const value = document.querySelector(".value");
+const btns = document.querySelectorAll(".btn");
+const pic = document.querySelector(".pic")
+
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const styles = event.currentTarget.classList;
+        if (styles.contains("btn-danger")) {
+            num--;
+        } else if (styles.contains("btn-success")) {
+            num++;
+        } else {
+            num = 0;
+        }
+        value.textContent = num;
+//Color de numeros
+        if (num > 0) {
+            value.style.color = "green";
+        } else if (num < 0) {
+            value.style.color = "red";
+        } else {
+            value.style.color = "black";
+        }
+//Emojis
+        if(num>0){
+            pic.src="img/feliz.png";
+        }else if(num<0){
+            pic.src="img/enojado.png"
+        }
+        else{
+            pic.src="img/timido.png"
+        }
+
+    });
+   
+
+});
